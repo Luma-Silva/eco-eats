80c3fbe18213485dbbbfdaba08fafefc218fa500
lana_verdete <verdetealana@gmail.com>
2023-07-31 19:37:11 -0300

    Mesclando lana/main

--- ecoEats/ecoEats/frmCadastroCnpj.cs
+++ ecoEats/ecoEats/frmCadastroCnpj.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ecoEats.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
@@ -17,158 +19,19 @@ namespace ecoEats
         {
             InitializeComponent();
         }
-
-        private void frmCadastroCnpj_Load(object sender, EventArgs e)
-        {
-
-        }
-        private void mkdTCnpj_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-            string cnpj = mkdTCnpj.Text;
-            if (cnpj == "")
-            {
-                mkdTCnpj.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                mkdTCnpj.BackColor = Color.White;
-
-            }
-        }
-
-        private void txtRazao_TextChanged(object sender, EventArgs e)
-        {
-            string razao = txtRazao.Text;
-            if (razao == "")
-            {
-                txtRazao.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtRazao.BackColor = Color.White;
-
-            }
-        }
-
-        private void txtName_TextChanged(object sender, EventArgs e)
-        {
-            string nome = txtName.Text;
-
-            if (nome == "")
-            {
-                txtName.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtName.BackColor = Color.White;
-            }
-        }
-
-        private void txtEndereco_TextChanged(object sender, EventArgs e)
-        {
-            string endereco = txtEndereco.Text;
-            if (endereco == "")
-            {
-                txtEndereco.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtEndereco.BackColor = Color.White;
-            }
-        }
-
-        private void txtNumero_TextChanged(object sender, EventArgs e)
-        {
-            string numero = txtNumero.Text;
-            if (numero == "")
-            {
-                txtNumero.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtNumero.BackColor = Color.White;
-            }
-        }
-
-        private void txtBairro_TextChanged(object sender, EventArgs e)
-        {
-            string bairro = txtBairro.Text;
-            if (bairro == "")
-            {
-                txtBairro.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtBairro.BackColor = Color.White;
-            }
-        }
-
-        private void mdkTCep_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-            string cep = mdkTCep.Text;
-            if (cep == "")
-            {
-                mdkTCep.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                mdkTCep.BackColor = Color.White;
-            }
-        }
-
-        private void txtEstado_TextChanged(object sender, EventArgs e)
-        {
-            string estado = txtEstado.Text;
-            if (estado == "")
-            {
-                txtEstado.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtEstado.BackColor = Color.White;
-            }
-        }
-
-        private void mkdTTelefone_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-            string telefone = mkdTTelefone.Text;
-            if (telefone == "")
-            {
-                mkdTTelefone.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                mkdTTelefone.BackColor = Color.White;
-            }
-        }
-
-        private void txtEmail_TextChanged(object sender, EventArgs e)
-        {
-            string email = txtEmail.Text;
-            if (email == "")
-            {
-                txtEmail.BackColor = Color.PaleVioletRed;
-            }
-            else
-            {
-                txtEmail.BackColor = Color.White;
-
-            }
-        }
-
         private void txtSenha_TextChanged(object sender, EventArgs e)
         {
 
             if (txtSenha.PasswordChar == '*')
             {
                 txtSenha.PasswordChar = '\0';
-                pbCSenha2.Image = Image.FromFile(@"..\..\Imagens\view.png");
+                pbCSenha2.Image = Resources.view;
                 txtSenha.UseSystemPasswordChar = false;
             }
             else
             {
                 txtSenha.PasswordChar = '*';
-                pbCSenha2.Image = Image.FromFile(@"..\..\Imagens\hide.png");
+                pbCSenha2.Image = Resources.hide;
                 txtSenha.UseSystemPasswordChar = false;
             }
         }
@@ -179,13 +42,13 @@ namespace ecoEats
             if (txtCSenha.PasswordChar == '*')
             {
                 txtCSenha.PasswordChar = '\0';
-                pbCSenha.Image = Image.FromFile(@"..\..\Imagens\view.png");
+                pbCSenha.Image = Resources.view;
                 txtCSenha.UseSystemPasswordChar = false;
             }
             else
             {
                 txtCSenha.PasswordChar = '*';
-                pbCSenha.Image = Image.FromFile(@"..\..\Imagens\hide.png");
+                pbCSenha.Image = Resources.hide;
                 txtCSenha.UseSystemPasswordChar = false;
             }
             if (txtCSenha.Text == txtSenha.Text)
@@ -194,10 +57,8 @@ namespace ecoEats
             }
 
         }
-
         private void btnCadastrar_Click(object sender, EventArgs e)
         {
-
             string cnpj = mkdTCnpj.Text;
             string razao = txtRazao.Text;
             string nome = txtName.Text;
@@ -211,20 +72,137 @@ namespace ecoEats
             string senha = txtSenha.Text;
             string confirmarsenha = txtCSenha.Text;
             string dados = rdbConfirmar.Text;
+            
             if (cnpj == "" || razao == "" || nome == "" || endereco == "" || numero == "" || bairro == "" || cep == "" || estado == "" || telefone == "" || email == "" || senha == "" || confirmarsenha == "")
             {
-                MessageBox.Show("Prencha todos os campos!");
+                if (cnpj == "")
+                {
+                    mkdTCnpj.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    mkdTCnpj.BackColor = Color.White;
+                }
+                if (razao == "")
+                {
+                    txtRazao.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtRazao.BackColor = Color.White;
+                }
+                if (nome == "")
+                {
+                    txtName.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtName.BackColor = Color.White;
+                }
+                if (endereco == "")
+                {
+                    txtEndereco.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtEndereco.BackColor = Color.White;
+                }
+                if (numero == "")
+                {
+                    txtNumero.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtNumero.BackColor = Color.White;
+                }
+                if (bairro == "")
+                {
+                    txtBairro.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtBairro.BackColor = Color.White;
+                }                
+                if (cep == "")
+                {
+                    mdkTCep.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    mdkTCep.BackColor = Color.White;
+                }
+                if (estado == "")
+                {
+                    txtEstado.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtEstado.BackColor = Color.White;
+                }
+                if (telefone == "")
+                {
+                    mkdTTelefone.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    mkdTTelefone.BackColor = Color.White;
+                }
+                if (email == "")
+                {
+                    txtEmail.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtEmail.BackColor = Color.White;
+
+                }
+                if (senha == "")
+                {
+                    txtSenha.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtSenha.BackColor = Color.White;
+
+                }
+                if (confirmarsenha == "")
+                {
+                    txtCSenha.BackColor = Color.PaleVioletRed;
+                }
+                else
+                {
+                    txtCSenha.BackColor = Color.White;
+
+                }
+                if (cnpj == "" || razao == "" || nome == "" || endereco == "" || numero == "" || bairro == "" || cep == "" || estado == "" || telefone == "" || email == "" || senha == "" || confirmarsenha == "")
+                    {
+                    MessageBox.Show("Preencha todos os campos!");
+                    }
             }
             else
-            {
+            {    
                 if (txtCSenha.Text != txtSenha.Text)
                 {
                     MessageBox.Show("Senhas diferentes!");
+                    BackColor = Color.PaleVioletRed;
                 }
-
                 else
                 {
                     MessageBox.Show("Cadastro concluído");
+                }
+            }
+        }
+
+        private void txtEmail_Validated(object sender, EventArgs e)
+        {
+
+            {
+                string email = txtEmail.Text;
+                Regex regex = new Regex(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$");
+                if (!regex.IsMatch(email))
+                {
+                    MessageBox.Show("Email inválido!");
+                    txtEmail.BackColor = Color.PaleVioletRed;
                 }
             }
         }
