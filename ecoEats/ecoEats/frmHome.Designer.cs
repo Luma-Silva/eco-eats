1782fc7193d3c77314ece13b3ead17a1873ea831
Luma-Silva <luizamaria.silva113@gmail.com>
2023-08-01 15:06:25 -0300

    Alterei algumas coisas na página principal. Criei a nossa Home page.

---
+++ ecoEats/ecoEats/frmHome.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ecoEats
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHome));
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.seusProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comprarSelosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.porNomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porCódigoDeBarrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.alterarInformaçõesPessoaisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(204)))), ((int)(((byte)(123)))));
+            this.menuStrip2.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Padding = new System.Windows.Forms.Padding(15, 12, 0, 12);
+            this.menuStrip2.Size = new System.Drawing.Size(925, 53);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.seusProdutosToolStripMenuItem,
+            this.comprarSelosToolStripMenuItem});
+            this.toolStripMenuItem3.Image = global::ecoEats.Properties.Resources.CapiIcon;
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(144, 29);
+            this.toolStripMenuItem3.Text = "Olá, [nome]";
+            // 
+            // seusProdutosToolStripMenuItem
+            // 
+            this.seusProdutosToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(209)))), ((int)(((byte)(154)))));
+            this.seusProdutosToolStripMenuItem.Image = global::ecoEats.Properties.Resources.new_features;
+            this.seusProdutosToolStripMenuItem.Name = "seusProdutosToolStripMenuItem";
+            this.seusProdutosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.seusProdutosToolStripMenuItem.Text = "Seus Produtos";
+            // 
+            // comprarSelosToolStripMenuItem
+            // 
+            this.comprarSelosToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(209)))), ((int)(((byte)(154)))));
+            this.comprarSelosToolStripMenuItem.Image = global::ecoEats.Properties.Resources.cart;
+            this.comprarSelosToolStripMenuItem.Name = "comprarSelosToolStripMenuItem";
+            this.comprarSelosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.comprarSelosToolStripMenuItem.Text = "Comprar Selos";
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.porNomeToolStripMenuItem,
+            this.porCódigoDeBarrasToolStripMenuItem});
+            this.toolStripMenuItem4.Image = global::ecoEats.Properties.Resources.grocery_bag;
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(197, 29);
+            this.toolStripMenuItem4.Text = "Cadastrar Produto";
+            // 
+            // porNomeToolStripMenuItem
+            // 
+            this.porNomeToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(209)))), ((int)(((byte)(154)))));
+            this.porNomeToolStripMenuItem.Image = global::ecoEats.Properties.Resources.signature;
+            this.porNomeToolStripMenuItem.Name = "porNomeToolStripMenuItem";
+            this.porNomeToolStripMenuItem.Size = new System.Drawing.Size(280, 34);
+            this.porNomeToolStripMenuItem.Text = "Por nome";
+            // 
+            // porCódigoDeBarrasToolStripMenuItem
+            // 
+            this.porCódigoDeBarrasToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(209)))), ((int)(((byte)(154)))));
+            this.porCódigoDeBarrasToolStripMenuItem.Image = global::ecoEats.Properties.Resources.barcode;
+            this.porCódigoDeBarrasToolStripMenuItem.Name = "porCódigoDeBarrasToolStripMenuItem";
+            this.porCódigoDeBarrasToolStripMenuItem.Size = new System.Drawing.Size(280, 34);
+            this.porCódigoDeBarrasToolStripMenuItem.Text = "Por código de barras";
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alterarInformaçõesPessoaisToolStripMenuItem});
+            this.toolStripMenuItem5.Image = global::ecoEats.Properties.Resources.gear;
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(166, 29);
+            this.toolStripMenuItem5.Text = "Configurações";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // alterarInformaçõesPessoaisToolStripMenuItem
+            // 
+            this.alterarInformaçõesPessoaisToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(209)))), ((int)(((byte)(154)))));
+            this.alterarInformaçõesPessoaisToolStripMenuItem.Image = global::ecoEats.Properties.Resources.change_management;
+            this.alterarInformaçõesPessoaisToolStripMenuItem.Name = "alterarInformaçõesPessoaisToolStripMenuItem";
+            this.alterarInformaçõesPessoaisToolStripMenuItem.Size = new System.Drawing.Size(343, 34);
+            this.alterarInformaçõesPessoaisToolStripMenuItem.Text = "Alterar Informações pessoais";
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.Image = global::ecoEats.Properties.Resources.power_on;
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(81, 29);
+            this.toolStripMenuItem6.Text = "Sair";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelPrincipal.AutoSize = true;
+            this.panelPrincipal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(247)))), ((int)(((byte)(236)))));
+            this.panelPrincipal.Location = new System.Drawing.Point(0, 56);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(925, 504);
+            this.panelPrincipal.TabIndex = 2;
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(247)))), ((int)(((byte)(236)))));
+            this.ClientSize = new System.Drawing.Size(925, 561);
+            this.Controls.Add(this.panelPrincipal);
+            this.Controls.Add(this.menuStrip2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmHome";
+            this.Text = "Home Page";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem seusProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comprarSelosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem porNomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porCódigoDeBarrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem alterarInformaçõesPessoaisToolStripMenuItem;
+        private System.Windows.Forms.Panel panelPrincipal;
+    }
+}
---
+++ ecoEats/ecoEats/frmHome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ecoEats
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
